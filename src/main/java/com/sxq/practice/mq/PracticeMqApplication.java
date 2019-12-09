620093f3151519cba083680f3ba46e4dae230f39
s-xq <971070045@qq.com>
2019-12-09 23:17:50 +0800

    [rocket-mq] add rocket mq dependency

---
+++ src/main/java/com/sxq/practice/mq/PracticeMqApplication.java
@@ -0,0 +1,13 @@
+package com.sxq.practice.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxq/practice/mq/PracticeMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxq.practice.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
